273cfbb6b46cc1501e2c2931fb5f51183b1b1490
skem7905 <skem7905@uni.sydney.edu.au>
2022-04-06 15:27:21 +1000

    Added my project files

---
+++ Sam's Code Assignment_2_1/Sources/main.c
@@ -0,0 +1,26 @@
+#include <hidef.h>      /* common defines and macros */
+#include "derivative.h"      /* derivative-specific definitions */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void main(void) {
+
+  char special_character = '.';
+  
+ 
+
+  
+  
+
+	EnableInterrupts;
+
+  for(;;) {
+    _FEED_COP(); /* feeds the dog */
+  } /* loop forever */
+  
+  
+  /* please make sure that you never leave main */
+}
